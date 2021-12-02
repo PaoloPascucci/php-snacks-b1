36f186de5894462fbf700303503e732f7a59335f
PaoloPascucci <pascuccip9@gmail.com>
2021-12-02 17:02:37 +0100

    Add snack 6

--- snack-6/index.php
+++ snack-6/index.php
@@ -1,5 +1,26 @@
 <?php
-
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +30,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>snack - 6</title>
+    <style>
+        .grey, .green{
+            color :white;
+        }
+            .grey{
+                background-color:gray;
+                height:50px;
+                width:200px
+            }
+            .green{
+                background-color:lime;
+                height:50px;
+                width:200px
+            }
+        </style>
 </head>
 <body>
+
     
+<div class="grey">
+    <?php
+        for ($i=0; $i < count($db['teachers']); $i++) { ?>
+        <p>
+            <?php echo $db['teachers'][$i]['name'];?> 
+            <?php echo $db['teachers'][$i]['lastname'];?>
+        </p>
+        <?php
+    };?>
+</div>
+<div class="green">
+        <?php
+        for ($i=0; $i < count($db['pm']); $i++) { ?>
+        <p>
+            <?php echo $db['pm'][$i]['name'];?> 
+            <?php echo $db['pm'][$i]['lastname'];?>
+        </p>
+        <?php
+    }; ?>
+</div>
 </body>
 </html>
