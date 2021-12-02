de27ed941cd72157784c0d1b374f2af29821b7e1
PaoloPascucci <pascuccip9@gmail.com>
2021-12-02 17:31:53 +0100

    Add Snack 7

--- snack-7/index.php
+++ snack-7/index.php
@@ -1,4 +1,21 @@
 <?php
+$Classe = [
+     [
+    'name' => 'Chiara',
+    'lastname' => 'Papagni',
+    'Voti' => [29,26,25]
+     ],
+     [
+    'name' => 'Marco',
+    'lastname' => 'Caliciotti',
+    'Voti' =>[26,27,28]
+     ]
+    ,[
+    'name' => 'Pablo',
+    'lastname' => 'Valerio Silva',
+    'Voti' => [28,21,24]
+    ],
+]
 
 ?>
 
@@ -12,5 +29,17 @@
 </head>
 <body>
     
+<div>
+      <?php
+        for ($i=0; $i < count($Classe); $i++) { ?>
+        <p>
+            <?php echo $Classe[$i]['name'];?> 
+            <?php echo $Classe[$i]['lastname'];?>
+            <?php echo round(array_sum($Classe[$i]['Voti'])/count($Classe[$i]['Voti']));?>
+        </p>
+        <?php
+    };?>
+</div>
+
 </body>
 </html>
